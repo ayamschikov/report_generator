b4a17c7424bf0d2ae8f9bdf3923b93059a2056d1
Aleksandr Yamschikov <yamshchikov.as73@gmail.com>
2019-12-09 16:49:47 +0400

    Reading file using batches

--- lib/parsers/session.rb
+++ lib/parsers/session.rb
@@ -3,11 +3,11 @@ module Parsers
     def self.parse(session)
       fields = session.split(',')
       {
-        'user_id' => fields[1],
-        'session_id' => fields[2],
-        'browser' => fields[3].upcase,
-        'time' => fields[4],
-        'date' => fields[5],
+        user_id: fields[1],
+        session_id: fields[2],
+        browser: fields[3].upcase,
+        time: fields[4],
+        date: fields[5],
       }
     end
   end
--- lib/parsers/user.rb
+++ lib/parsers/user.rb
@@ -3,10 +3,10 @@ module Parsers
     def self.parse(user)
       fields = user.split(',')
       {
-        'id' => fields[1],
-        'first_name' => fields[2],
-        'last_name' => fields[3],
-        'age' => fields[4],
+        id: fields[1],
+        first_name: fields[2],
+        last_name: fields[3],
+        age: fields[4],
       }
     end
   end
