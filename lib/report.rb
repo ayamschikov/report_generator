a107acdc56f0283d3b1c32ed1fa279dcc1f5f4b3
Aleksandr Yamschikov <yamshchikov.as73@gmail.com>
2019-12-10 09:52:54 +0400

    Rename create report method

--- lib/report.rb
+++ lib/report.rb
@@ -3,22 +3,18 @@
 module Report
   module_function
 
-  def create(report, user, sessions)
+  def collect_stats_from_sessions(sessions)
     sessions_time = sessions.map { |s| s[:time].to_i }
     browsers = sessions.map { |s| s[:browser] }
 
-    user_key = user[:first_name].to_s + ' ' + user[:last_name].to_s
-    report[:usersStats][user_key] =
-      {
-        sessionsCount: sessions.count,
-        totalTime: sessions_time.sum.to_s + ' min.',
-        longestSession: sessions_time.max.to_s + ' min.',
-        browsers: browsers.sort.join(', '),
-        usedIE: browsers.any? { |b| b =~ /INTERNET EXPLORER/ },
-        alwaysUsedChrome: browsers.all? { |b| b =~ /CHROME/ },
-        dates: sessions.map { |s| s[:date] }.sort.reverse
-      }
-
-    report
+    {
+      sessionsCount: sessions.count,
+      totalTime: sessions_time.sum.to_s + ' min.',
+      longestSession: sessions_time.max.to_s + ' min.',
+      browsers: browsers.sort.join(', '),
+      usedIE: browsers.any? { |b| b =~ /INTERNET EXPLORER/ },
+      alwaysUsedChrome: browsers.all? { |b| b =~ /CHROME/ },
+      dates: sessions.map { |s| s[:date] }.sort.reverse
+    }
   end
 end
