5f33eee7a8c6e2189957fd87898fffb67ef9fe2c
Aleksandr Yamschikov <yamshchikov.as73@gmail.com>
2019-12-10 23:26:02 +0400

    Final results

--- lib/app.rb
+++ lib/app.rb
@@ -7,7 +7,6 @@ class App
     report = {}
     user = nil
     sessions = []
-    batch_size = 20000
     rep_limit = 500
     user_stats = []
     need_comma = false
@@ -29,30 +28,25 @@ class App
     # determine offset where to write userStats
     offset = report.to_json.length - 2
 
-    File.open(source, 'r') do |file|
-      file.lazy.each_slice(batch_size) do |lines|
+    File.foreach(source) do |line|
+      fields = line.split(',')
 
-        lines.each do |line|
-          fields = line.split(',')
-
-          case fields[0]
-          when 'user'
-            unless sessions.empty?
-              user_key = "#{user[:first_name]} #{user[:last_name]}"
-              user_stats << "\"#{user_key}\":#{Report.collect_stats_from_sessions(sessions).to_json}"
-              sessions = []
-            end
-            user = Parsers::User.parse(fields)
-          when 'session'
-            sessions.push Parsers::Session.parse(fields)
-          end
+      case fields[0]
+      when 'user'
+        unless sessions.empty?
+          user_key = "#{user[:first_name]} #{user[:last_name]}"
+          user_stats << "\"#{user_key}\":#{Report.collect_stats_from_sessions(sessions).to_json}"
+          sessions = []
         end
+        user = Parsers::User.parse(fields)
+      when 'session'
+        sessions.push Parsers::Session.parse(fields)
+      end
 
-        if user_stats.count == rep_limit
-          need_comma = true
-          offset += IO.binwrite(dest, user_stats.join(','), offset)
-          user_stats = []
-        end
+      if user_stats.count == rep_limit
+        need_comma = true
+        offset += IO.binwrite(dest, user_stats.join(','), offset)
+        user_stats = []
       end
     end
 
